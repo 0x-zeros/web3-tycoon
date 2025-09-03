013ab5b5275f948c96a1c650bbde259557ea7593
zeros <zeros.null@gmail.com>
2025-09-03 12:17:37 +0800

    refactor(old): 为重构代码做准备

--- client/tycoon_cocos/assets/scripts/i18n/index.ts
+++
@@ -1,8 +0,0 @@
-// i18n module exports
-export { default as i18n } from './i18n';
-export { default as Polyglot } from './polyglot';
-export { LabelLocalized } from './LabelLocalized';
-export { I18nTest } from './i18n-test';
-
-// Type exports
-export type { LanguageData, SupportedLanguage, TranslationOptions } from './i18n';

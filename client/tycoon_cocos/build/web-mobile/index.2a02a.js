1b80d0db16a3fc3f88548200a08c44137143ba35
zeros <zeros.null@gmail.com>
2025-10-17 23:03:43 +0800

    build: 更新构建文件

--- client/tycoon_cocos/build/web-mobile/index.2a02a.js
+++ client/tycoon_cocos/build/web-mobile/index.2a02a.js
@@ -1,4 +1,4 @@
-System.register(["./application.22792.js"], function (_export, _context) {
+System.register(["./application.eea7f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

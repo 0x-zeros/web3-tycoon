0a86f4f303478ee214cd1744d8157379bae178ab
zeros <zeros.null@gmail.com>
2025-10-17 21:28:49 +0800

    build(builder):  尝试 把resources 整个做成远程bundle

--- client/tycoon_cocos/build/web-mobile/index.743e0.js
+++ client/tycoon_cocos/build/web-mobile/index.743e0.js
@@ -1,4 +1,4 @@
-System.register(["./application.0a82e.js"], function (_export, _context) {
+System.register(["./application.22792.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

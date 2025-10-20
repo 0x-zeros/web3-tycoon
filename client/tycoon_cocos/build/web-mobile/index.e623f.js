ba738f959af480c30093c7a4e296c9f99f8556d8
zeros <zeros.null@gmail.com>
2025-10-21 00:41:53 +0800

    build: 更新构建文件

--- client/tycoon_cocos/build/web-mobile/index.e623f.js
+++ client/tycoon_cocos/build/web-mobile/index.e623f.js
@@ -1,4 +1,4 @@
-System.register(["./application.8ac48.js"], function (_export, _context) {
+System.register(["./application.421f7.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

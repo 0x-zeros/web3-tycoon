84cb080d45dca158374bad6356ee4462d29a6cc7
zeros <zeros.null@gmail.com>
2025-10-21 09:50:04 +0800

    build: 更新构建文件(walrus)

--- client/tycoon_cocos/build/web-walrus/index.4ee49.js
+++ client/tycoon_cocos/build/web-walrus/index.4ee49.js
@@ -1,4 +1,4 @@
-System.register(["./application.74a5e.js"], function (_export, _context) {
+System.register(["./application.6a10f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;

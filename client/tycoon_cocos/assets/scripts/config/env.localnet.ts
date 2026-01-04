7d85168e1bafe7b0afdaedec642cc8d25c9d3e74
zeros <zeros.null@gmail.com>
2026-01-04 23:04:25 +0800

    feat(ui): 更新卡牌列表组件，支持横向滚动和自动清理项目

--- client/tycoon_cocos/assets/scripts/config/env.localnet.ts
+++ client/tycoon_cocos/assets/scripts/config/env.localnet.ts
@@ -1,12 +1,12 @@
 /**
  * Sui区块链环境配置
- * 自动生成于: 2026-01-04 17:12:40
+ * 自动生成于: 2026-01-04 22:55:25
  */
 export const SuiEnvConfig = {
-    packageId: '0x835a7e5fd48bea93600126d8855b92465c94691e4af281ad3b8ac80bbb274af4',
-    upgradeCap: '0x24ff262978f14b4379cf7bc399e06a7f4a5a0fe0926ae00446fcff08f33640e4',
-    adminCap: '0xfd0d917fe4f9fa2f0c89e2e65e7becdd1f204799dd97168e522342db589429d4',
-    gameData: '0x9bf03bed6569a55ef1806fa78196129360cc26ba9ec1a648d84aee324071bf12',
+    packageId: '0x0fcdec133cc0160fd08e81027504bfd8199e1cf8c23ef4dd8770e08b484ef1eb',
+    upgradeCap: '0x9b74bece8faccd2470033e3ea5bf63d6443409033979e39e3205d241912b335f',
+    adminCap: '0xe6806001fb62745a57389a77939b7906d8ca5fa34515caacb83905012b6da29e',
+    gameData: '0x52604a761af512d064cb1a56fd6e4a2c0fc49eae6feee02710d77a5036a3f0c8',
     network: 'localnet',
 
     // 开发环境使用 Keypair 签名

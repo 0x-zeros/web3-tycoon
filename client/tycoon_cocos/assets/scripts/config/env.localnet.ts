f49f9031561a5324a53f7cca5caca930aadd2572
zeros <zeros.null@gmail.com>
2026-01-04 08:18:15 +0800

    fix(config): 更新本地网络环境配置和Move.lock中的合约ID

--- client/tycoon_cocos/assets/scripts/config/env.localnet.ts
+++ client/tycoon_cocos/assets/scripts/config/env.localnet.ts
@@ -1,12 +1,12 @@
 /**
  * Sui区块链环境配置
- * 自动生成于: 2026-01-03 08:33:29
+ * 自动生成于: 2026-01-04 08:14:38
  */
 export const SuiEnvConfig = {
-    packageId: '0x49a7159f6199d9ae4c4da894f7c8d599777d511db7ad51b61caf1d4eae26a1a5',
-    upgradeCap: '0x232d5c81711e8ec7b123b983fc04898f31db4dc9c7dc289879ae30a45d7b646e',
-    adminCap: '0xb030fc24cc558f667763649a625054ab923938c0035b4f1aa3334f5c80e450ae',
-    gameData: '0xb270a644f9ad053331dd7af9645f0e2fe1f8c14f44c76af613203d5bf3704277',
+    packageId: '0x7bf7adf9cfabc8f1098fcd65c8aff62c2d00ed23d846f165cba41e72f02ef90e',
+    upgradeCap: '0x1fd33a3b40afcd14b8d2ce8135a6ff10f323465e06bf5541430e4ac2a53cc648',
+    adminCap: '0xa023f59e6448dd08c3e35d951101c98dd97ee24f7830c12655a9dd62b592e17b',
+    gameData: '0x33c918a7512868657b637343c088b24fad65986856e65590270f31f7af663e0d',
     network: 'localnet',
 
     // 开发环境使用 Keypair 签名

3883ab9b87dfb3653a211fb02c61f6e3c6c3b7b4
zeros <zeros.null@gmail.com>
2026-01-05 08:01:38 +0800

    fix(asset): 更新卡牌纹理的包裹模式为 clamp-to-edge

--- client/tycoon_cocos/assets/scripts/config/env.localnet.ts
+++ client/tycoon_cocos/assets/scripts/config/env.localnet.ts
@@ -1,12 +1,12 @@
 /**
  * Sui区块链环境配置
- * 自动生成于: 2026-01-04 22:55:25
+ * 自动生成于: 2026-01-04 23:10:29
  */
 export const SuiEnvConfig = {
-    packageId: '0x0fcdec133cc0160fd08e81027504bfd8199e1cf8c23ef4dd8770e08b484ef1eb',
-    upgradeCap: '0x9b74bece8faccd2470033e3ea5bf63d6443409033979e39e3205d241912b335f',
-    adminCap: '0xe6806001fb62745a57389a77939b7906d8ca5fa34515caacb83905012b6da29e',
-    gameData: '0x52604a761af512d064cb1a56fd6e4a2c0fc49eae6feee02710d77a5036a3f0c8',
+    packageId: '0xfb697a79a6fbb6a0058c12924971317593e2f90213035c0452a60118573377a7',
+    upgradeCap: '0xc575bdc2beca6190ac3bec18069108a08f4bbc0f86fcb69591fc164e3e4cbf56',
+    adminCap: '0x150b52436941fbb1d49c748a40d7bcf820e4c154974b3cafa18bfd51a6a82d12',
+    gameData: '0xa71cc923253a0a1478c1039c24ce95a7bfe3fc4d918b7958487aa3fd0c9dce76',
     network: 'localnet',
 
     // 开发环境使用 Keypair 签名
